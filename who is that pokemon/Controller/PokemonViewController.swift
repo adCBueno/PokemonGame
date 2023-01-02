f96f59e6b1c5d92ddb6d256b07f576466ceefdce
adriana <adrianacc.bueno@gmail.com>
2023-01-02 18:14:09 -0400

    Delegate correction

--- who is that pokemon/Controller/PokemonViewController.swift
+++ who is that pokemon/Controller/PokemonViewController.swift
@@ -5,11 +5,18 @@ class PokemonViewController: UIViewController {
     @IBOutlet weak var pokemonImage: UIImageView!
     @IBOutlet weak var labelScore: UILabel!
     @IBOutlet weak var labelMessage: UILabel!
-        
     @IBOutlet var answerButtons: [UIButton]!
+    
+    lazy var pokemonManager = PokemonManager()
+    
+    var random4Pokemons: [PokemonModel] = []
+    var correctAnswer: String = ""
+    var correctAnswerImage: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pokemonManager.delegate = self
+        pokemonManager.fetchPokemon()
         
         createButtons()
     }
@@ -20,6 +27,39 @@ class PokemonViewController: UIViewController {
     }
     
     func createButtons() {
+        for button in answerButtons {
+            button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.5).cgColor
+            button.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+            button.layer.shadowOpacity = 1.0
+            button.layer.shadowRadius = 0
+            button.layer.masksToBounds = false
+            button.layer.cornerRadius = 10.0
+        }
+    }
+}
+
+extension PokemonViewController: PokemonManagerDelegate {
+    func didUpdatePokemon(pokemons: [PokemonModel]) {
+        random4Pokemons = pokemons.choose(n: 4)
         
+        let index = Int.random(in: 0...3)
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
+}
+
+// Colecciones disponibvles en XCode, arrays, colecciones, etc. Vamos a iterar a travez de indices, los iteraremos
+extension Collection where Indices.Iterator.Element == Index {
+    // Safe para que no se vaya a indices que no existen
+    public subscript(safe index: Index) -> Iterator.Element? {
+        return (startIndex <= index && index < endIndex) ? self[index] : nil
+    }
+}
+
+extension Collection {
+    func choose(n: Int) -> Array<Element> {
+        Array(shuffled().prefix(n))
     }
 }
